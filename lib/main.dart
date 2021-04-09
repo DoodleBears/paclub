1b60ba4e833a568a20cfb666540d5833e7260a10
DoodleBear <yangmufeng233@gmail.com>
2021-04-09 16:33:26 +0800

    chore: add logger dependency to make cleaner log messages, and for better debug

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
 import 'pages/Tabs.dart';
+import 'package:logger/logger.dart';
+
+var logger = Logger(
+  printer: PrettyPrinter(),
+);
+
+var loggerNoStack = Logger(
+  printer: PrettyPrinter(methodCount: 0),
+);
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    logger.d(
+        'Click here to checkout more Logger utility https://pub.dev/packages/logger/example');
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         title: "Welcome to 盒群",
