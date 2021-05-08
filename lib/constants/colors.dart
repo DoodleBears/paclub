908a2c0252d2bd5fc3abff7471f2b467285bbbbc
DoodleBear <yangmufeng233@gmail.com>
2021-05-08 23:08:36 +0800

    refactor: separate const value(color, number...) used in project to constants folder

---
+++ lib/constants/colors.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/widgets.dart';
+
+// App主题，包含: 颜色，字体大小等
+const accentColor = Color(0xFF96C336);
+const accentLightColor = Color(0xFFB3D270);
+const accentDarkColor = Color(0xFF75A118);
+const primaryColor = Color(0xFFF0BF84);
+const primaryLightColor = Color(0xFFFCF4E9);
+const primaryDarkColor = Color(0xFF9F7C53);
+// const primaryTextColor = Color(0xFFF1E6FF);
+// const secondaryTextColor = Color(0xFFF1E6FF);
+
+Color hexToColor(String hex) {
+  assert(RegExp(r'^#([0-9a-fA-F]{6})|([0-9a-fA-F]{8})$').hasMatch(hex),
+      'hex color must be #rrggbb or #rrggbbaa');
+
+  return Color(
+    int.parse(hex.substring(1), radix: 16) +
+        (hex.length == 7 ? 0xff000000 : 0x00000000),
+  );
+}
--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -1,15 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-
-// App主题，包含: 颜色，字体大小等
-const accentColor = Color(0xFF96C336);
-const accentLightColor = Color(0xFFB3D270);
-const accentDarkColor = Color(0xFF75A118);
-const primaryColor = Color(0xFFF0BF84);
-const primaryLightColor = Color(0xFFFCF4E9);
-const primaryDarkColor = Color(0xFF9F7C53);
-// const primaryTextColor = Color(0xFFF1E6FF);
-// const secondaryTextColor = Color(0xFFF1E6FF);
+import 'package:paclub/constants/constants.dart';
 
 ThemeData get appThemeData => ThemeData(
       primaryColor: primaryColor,
