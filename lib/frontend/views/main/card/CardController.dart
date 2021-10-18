24688ba3b9d74db2775c906f5ba7c39700397712
vivian-create <vivianop890528@gmail.com>
2021-10-18 23:43:34 +0800

    fix: try to fix card

--- lib/frontend/views/main/card/CardController.dart
+++ lib/frontend/views/main/card/CardController.dart
@@ -1,18 +1,18 @@
 import 'package:get/get.dart';
 import 'package:paclub/utils/logger.dart';
-
-class CardController extends GetxController {
+class CardController extends GetxController  {
   String testString = '这是从controller获得的string';
+  
 
   @override
   void onInit() {
-    logger.i('启用 CardController');
+    logger.i('启用 DragController');
     super.onInit();
   }
 
   @override
   void onClose() {
-    logger.w('关闭 CardController');
+    logger.w('关闭 DragController');
     super.onClose();
   }
 }
