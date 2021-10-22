951d6f496423b5207ac0a9c030fcd8f660584c25
DoodleBear <yangmufeng233@gmail.com>
2021-10-23 02:23:57 +0800

    fix: SaveArea 包裹后, 其之外的部分颜色没有跟随系统变化同步

--- lib/frontend/views/main/tabs/tabs_page.dart
+++ lib/frontend/views/main/tabs/tabs_page.dart
@@ -36,105 +36,107 @@ class Tabs extends GetView<TabsController> {
         }
         return false;
       },
-      child: SafeArea(
-        top: false,
-        bottom: true,
-        child: GetBuilder<AppController>(builder: (_) {
-          return GetBuilder<TabsController>(builder: (_) {
-            return Scaffold(
-              backgroundColor: AppColors.bottomNavigationBarBackgroundColor,
-              body: pageList[
-                  controller.currentIndex], // 从controller 得知当前应该显示哪个page
-              bottomNavigationBar: Stack(
-                children: [
-                  BottomNavigationBar(
-                    enableFeedback: false,
-                    showUnselectedLabels: false,
-                    currentIndex: controller.currentIndex,
-                    elevation: 0.0,
-                    backgroundColor: Colors.transparent,
+      child: GetBuilder<AppController>(builder: (_) {
+        return Container(
+          color: AppColors.bottomNavigationBarBackgroundColor,
+          child: SafeArea(
+            top: false,
+            child: GetBuilder<TabsController>(builder: (_) {
+              return Scaffold(
+                backgroundColor: AppColors.bottomNavigationBarBackgroundColor,
+                body: pageList[
+                    controller.currentIndex], // 从controller 得知当前应该显示哪个page
+                bottomNavigationBar: Stack(
+                  children: [
+                    BottomNavigationBar(
+                      enableFeedback: false,
+                      showUnselectedLabels: false,
+                      currentIndex: controller.currentIndex,
+                      elevation: 0.0,
+                      backgroundColor: Colors.transparent,
 
-                    onTap: (int index) {
-                      controller.setIndex(index);
-                    },
+                      onTap: (int index) {
+                        controller.setIndex(index);
+                      },
 
-                    showSelectedLabels: false,
-                    selectedItemColor: AppColors.bottomNavigationBarTabColor,
-                    type: BottomNavigationBarType.fixed,
-                    iconSize: Get.width * 0.07,
-                    // unselectedLabelStyle:
-                    //     TextStyle(fontSize: 14.0, fontWeight: FontWeight.bold),
-                    // selectedLabelStyle:
-                    //     TextStyle(fontSize: 14.0, fontWeight: FontWeight.bold),
-                    items: [
-                      BottomNavigationBarItem(
-                        icon: Icon(Icons.home_outlined),
-                        activeIcon: Icon(Icons.home),
-                        label: '首頁',
-                      ),
-                      BottomNavigationBarItem(
-                        icon: Icon(Icons.star_outline),
-                        activeIcon: Icon(Icons.star),
-                        label: '抽卡',
-                      ),
-                      BottomNavigationBarItem(
-                        icon: Stack(
-                          clipBehavior: Clip.none,
-                          children: [
-                            Icon(Icons.mail_outline),
-                            Positioned(
-                              top: -6.0,
-                              right: -10.0,
-                              child: GetBuilder<AppController>(
-                                builder: (_) {
-                                  return _buildBadge(userController);
-                                },
-                              ),
-                            )
-                          ],
+                      showSelectedLabels: false,
+                      selectedItemColor: AppColors.bottomNavigationBarTabColor,
+                      type: BottomNavigationBarType.fixed,
+                      iconSize: Get.width * 0.07,
+                      // unselectedLabelStyle:
+                      //     TextStyle(fontSize: 14.0, fontWeight: FontWeight.bold),
+                      // selectedLabelStyle:
+                      //     TextStyle(fontSize: 14.0, fontWeight: FontWeight.bold),
+                      items: [
+                        BottomNavigationBarItem(
+                          icon: Icon(Icons.home_outlined),
+                          activeIcon: Icon(Icons.home),
+                          label: '首頁',
                         ),
-                        activeIcon: Stack(
-                          clipBehavior: Clip.none,
-                          children: [
-                            Icon(Icons.mail),
-                            Positioned(
-                              right: -10.0,
-                              top: -6.0,
-                              child: GetBuilder<AppController>(
-                                builder: (_) {
-                                  return _buildBadge(userController);
-                                },
-                              ),
-                            )
-                          ],
+                        BottomNavigationBarItem(
+                          icon: Icon(Icons.star_outline),
+                          activeIcon: Icon(Icons.star),
+                          label: '抽卡',
                         ),
-                        label: '私訊',
-                      ),
-                      BottomNavigationBarItem(
-                        icon: Icon(Icons.notifications_outlined),
-                        activeIcon: Icon(Icons.notifications),
-                        label: '提醒',
-                      ),
-                      BottomNavigationBarItem(
-                        icon: Icon(Icons.person_outline),
-                        activeIcon: Icon(Icons.person),
-                        label: '個人',
-                      ),
-                    ],
-                  ),
-                  // 顶部黑色线条
-                  Positioned(
-                    child: Container(
-                      height: 1.5,
-                      color: AppColors.divideLineColor,
+                        BottomNavigationBarItem(
+                          icon: Stack(
+                            clipBehavior: Clip.none,
+                            children: [
+                              Icon(Icons.mail_outline),
+                              Positioned(
+                                top: -6.0,
+                                right: -10.0,
+                                child: GetBuilder<AppController>(
+                                  builder: (_) {
+                                    return _buildBadge(userController);
+                                  },
+                                ),
+                              )
+                            ],
+                          ),
+                          activeIcon: Stack(
+                            clipBehavior: Clip.none,
+                            children: [
+                              Icon(Icons.mail),
+                              Positioned(
+                                right: -10.0,
+                                top: -6.0,
+                                child: GetBuilder<AppController>(
+                                  builder: (_) {
+                                    return _buildBadge(userController);
+                                  },
+                                ),
+                              )
+                            ],
+                          ),
+                          label: '私訊',
+                        ),
+                        BottomNavigationBarItem(
+                          icon: Icon(Icons.notifications_outlined),
+                          activeIcon: Icon(Icons.notifications),
+                          label: '提醒',
+                        ),
+                        BottomNavigationBarItem(
+                          icon: Icon(Icons.person_outline),
+                          activeIcon: Icon(Icons.person),
+                          label: '個人',
+                        ),
+                      ],
                     ),
-                  ),
-                ],
-              ),
-            );
-          });
-        }),
-      ),
+                    // 顶部黑色线条
+                    Positioned(
+                      child: Container(
+                        height: 1.5,
+                        color: AppColors.divideLineColor,
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            }),
+          ),
+        );
+      }),
     );
   }
 
